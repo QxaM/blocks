e5fc069f46e59ec2d9f72c90e51469b03e4e0e58
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2023-07-05 20:55:06 +0200

    Wall implemented to use CompositeBlock with downcasting

--- src/main/java/org/structures/wall/Wall.java
+++ src/main/java/org/structures/wall/Wall.java
@@ -1,6 +1,9 @@
 package org.structures.wall;
 
 import org.structures.blocks.Block;
+import org.structures.blocks.Brick;
+import org.structures.blocks.composites.CompositeBlock;
+import org.structures.blocks.composites.CompositeBrick;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,19 +21,28 @@ public class Wall implements Structure {
     @Override
     public Optional<Block> findBlockByColor(String color) {
         return blocks.stream()
-                .filter(block -> block.getColor().equals(color))
+                .filter(block -> block.getColor().contains(color))
                 .findAny();
     }
 
     @Override
     public List<Block> findBlocksByMaterial(String material) {
         return blocks.stream()
-                .filter(block -> block.getMaterial().equals(material))
+                .filter(block -> block.getMaterial().contains(material))
                 .collect(Collectors.toList());
     }
 
     @Override
     public int count() {
-        return blocks.size();
+        int count = 0;
+        for (Block block: blocks) {
+            if (block instanceof CompositeBrick) {
+                CompositeBlock compositeBlock = (CompositeBrick) block;
+                count += compositeBlock.getBlocks().size();
+            } else {
+                count++;
+            }
+        }
+        return count;
     }
 }
--- src/test/java/WallTests.java
+++ src/test/java/WallTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.structures.blocks.Block;
 import org.structures.blocks.Brick;
+import org.structures.blocks.composites.CompositeBrick;
 import org.structures.wall.Wall;
 
 import java.util.List;
@@ -16,6 +17,18 @@ public class WallTests {
         Brick brick3 = new Brick("red", "stone");
         Brick brick4 = new Brick("white", "cement");
         return List.of(brick1, brick2, brick3, brick4);
+    }
+
+    private CompositeBrick initCompositeBlock() {
+        CompositeBrick compositeBrick = new CompositeBrick();
+
+        Brick brick1 = new Brick("red", "clay");
+        Brick brick2 = new Brick("gray", "cement");
+        Brick brick3 = new Brick("red", "stone");
+        Brick brick4 = new Brick("white", "cement");
+
+        compositeBrick.getBlocks().addAll(List.of(brick1, brick2, brick3, brick4));
+        return compositeBrick;
     }
 
     @Test
@@ -58,5 +71,108 @@ public class WallTests {
 
         //Then
         assertEquals(4, blockCount);
+    }
+
+    @Test
+    void testEmptyBlocks() {
+        //Given
+        Wall wall = new Wall();
+
+        //When
+        Optional<Block> foundByColor = wall.findBlockByColor("red");
+        List<Block> foundByMaterial = wall.findBlocksByMaterial("cement");
+        int count = wall.count();
+
+        //Then
+        assertAll(() -> assertTrue(foundByColor.isEmpty()),
+                () -> assertTrue(foundByMaterial.isEmpty()),
+                () -> assertEquals(0, count));
+    }
+
+    @Test
+    void testShouldFindByColor_WithCompositeBlocks() {
+        //Given
+        Wall wall = new Wall();
+        wall.getBlocks().add(initCompositeBlock());
+
+        //When
+        Optional<Block> foundBlock = wall.findBlockByColor("red");
+
+        //Then
+        assertTrue(foundBlock.isPresent());
+        assertEquals("red, gray, white", foundBlock.get().getColor());
+    }
+
+    @Test
+    void testShouldFindByMaterial_WithCompositeBlocks() {
+        //Given
+        Wall wall = new Wall();
+        wall.getBlocks().add(initCompositeBlock());
+
+        //When
+        List<Block> foundBlocks = wall.findBlocksByMaterial("cement");
+
+        //Then
+        assertAll(() -> assertEquals(1, foundBlocks.size()),
+                () -> assertEquals("clay, cement, stone", foundBlocks.get(0).getMaterial()));
+    }
+
+    @Test
+    void testShouldGetCount_WithCompositeBlocks() {
+        //Given
+        Wall wall = new Wall();
+        wall.getBlocks().add(initCompositeBlock());
+
+        //When
+        int blockCount = wall.count();
+
+        //Then
+        assertEquals(4, blockCount);
+    }
+
+    @Test
+    void testShouldFindByColor_WithMixedBlocks() {
+        //Given
+        Wall wall = new Wall();
+        wall.getBlocks().add(initCompositeBlock());
+        wall.getBlocks().addAll(initBlocks());
+
+        //When
+        Optional<Block> foundBlock = wall.findBlockByColor("red");
+
+        //Then
+        assertTrue(foundBlock.isPresent());
+        assertTrue(foundBlock.get().getColor().contains("red"));
+    }
+
+    @Test
+    void testShouldFindByMaterial_WithMixedBlocks() {
+        //Given
+        Wall wall = new Wall();
+        wall.getBlocks().add(initCompositeBlock());
+        wall.getBlocks().addAll(initBlocks());
+
+        //When
+        List<Block> foundBlocks = wall.findBlocksByMaterial("cement");
+
+        //Then
+        assertEquals(3, foundBlocks.size());
+        assertAll(() -> assertTrue(foundBlocks.get(0).getMaterial().contains("cement")),
+                () -> assertTrue(foundBlocks.get(1).getMaterial().contains("cement")),
+                () -> assertTrue(foundBlocks.get(2).getMaterial().contains("cement")));
+    }
+
+    @Test
+    void testShouldGetCount_WithMixedBlocks() {
+        //Given
+        Wall wall = new Wall();
+        wall.getBlocks().add(initCompositeBlock());
+        wall.getBlocks().addAll(initBlocks());
+
+        //When
+        int blockCount = wall.count();
+
+        //Then
+        assertEquals(8, blockCount);
     }
 }
